36cb7e210ccd66d236a9fe98aa88165e428c1c29
moja <156500521+moja-dev@users.noreply.github.com>
2024-09-15 22:25:47 +0330

    Update config.py : unexpected indent

--- bot/config/config.py
+++ bot/config/config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
     AUTO_TASK: bool = True
     AUTO_CLAIM_SQUAD_BONUS: bool = False
 
-     RANDOM_DELAY_IN_RUN: list = [0, 25]
+    RANDOM_DELAY_IN_RUN: list = [0, 25]
     
     USE_PROXY_FROM_FILE: bool = False
 
